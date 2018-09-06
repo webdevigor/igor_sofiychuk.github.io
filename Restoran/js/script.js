de0ab479ac4e8312e3908296919974bc0b787943
Игорь Софийчук <36086382+webdevigor@users.noreply.github.com>
2018-09-06 22:03:17 +0300

    Add files via upload

---
+++ Restoran/js/script.js
@@ -0,0 +1,11 @@
+$('.slider-slick').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 300,
+  arrows: false,
+  dots: true,
+  autoplay: true
+});
+
+console.log("sadsa");
